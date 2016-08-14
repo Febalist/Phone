7fad49f64a67c2d9d29c11121c0e741feb6dc440
Febalist <febalist@gmail.com>
2016-08-14 17:53:45 +0300

    Init

---
+++ src/Phone.php
@@ -0,0 +1,6 @@
+<?php namespace Febalist\Phone;
+
+class Phone
+{
+
+}
---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!function_exists('phone_pretty')) {
+    function phone_pretty()
+    {
+        return null;
+    }
+}
+
+if (!function_exists('phone_parse')) {
+    function phone_parse()
+    {
+        return null;
+    }
+}
